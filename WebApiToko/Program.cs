1a1a919935a1d151d701473811cd9160e222d33c
sutannnn <135008999+sutannnn@users.noreply.github.com>
2026-01-05 22:35:14 +0700

    migration dan scaffolding dan identity bawaan dotnet

--- WebApiToko/Program.cs
+++ WebApiToko/Program.cs
@@ -1,12 +1,18 @@
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.Identity.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebApiToko.ModelsEF.Toko;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Konfigurasi DBContext ke SQL Server/LocalDB
+builder.Services.AddDbContext<TokoDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Tambahkan Identity
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<TokoDbContext>();
+
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
