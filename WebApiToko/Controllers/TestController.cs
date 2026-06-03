52bb32b7f2713a2545c776d1f6ae4812ffb4df91
sutannnn <135008999+sutannnn@users.noreply.github.com>
2026-06-03 11:33:47 +0700

    Revert "Test"

--- WebApiToko/Controllers/TestController.cs
+++ WebApiToko/Controllers/TestController.cs
@@ -11,9 +11,7 @@ namespace WebApiToko.Controllers
         [Authorize]
         public IActionResult Index()
         {
-            // commit 1
             return Ok("Success");
-            // commit 3
         }
     }
 }
